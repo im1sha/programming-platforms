b53074c4aa65acd51ab4013b20728aaced991d47
im1sha <vkporsche@ya.ru>
2018-09-25 19:43:28 +0300

    Mutex class without realizaiton

---
+++ Mutex/Mutex/Mutex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mutex
+{
+    public class Mutex
+    {
+        public Mutex() { }
+
+        public void Lock()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unlock()
+        {            
+            throw new NotImplementedException();
+        }
+    }
+}
