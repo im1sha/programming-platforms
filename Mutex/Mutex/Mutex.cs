643e2144ea3e89206c2129cb05e69cc715d48c87
im1sha <vkporsche@ya.ru>
2018-09-26 15:25:52 +0300

    Mutex realization

--- Mutex/Mutex/Mutex.cs
+++ Mutex/Mutex/Mutex.cs
@@ -1,23 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Mutex
 {
+    /// <summary>
+    /// A synchronization primitive
+    /// </summary>
     public class Mutex
     {
-        public Mutex() { }
+        /// <summary>
+        /// Synchronization variable 
+        /// </summary> 
+        private static int UsingResource;
 
-        public void Lock()
+        private static readonly int InUse = 1;
+        private static readonly int NotUsed = 0;
+
+        public Mutex() : this(false) { }
+
+        /// <summary>
+        /// Constructor that gets initial instance state 
+        /// </summary>
+        /// <param name="initiallyOwned">Initial mutex state</param>
+        public Mutex(bool initiallyOwned)
         {
-            throw new NotImplementedException();
+            UsingResource = initiallyOwned ? InUse : NotUsed;
         }
 
+        /// <summary>
+        /// Blocks the current thread until the current Mutex instance receives a signal
+        /// </summary>
+        public void Lock()
+        {
+            int initialValue;
+            do
+            {
+                initialValue = UsingResource;               
+            } while (initialValue == InUse || initialValue != Interlocked.CompareExchange(ref UsingResource, InUse, initialValue));
+        }
+
+        /// <summary>
+        /// Releases a mutex
+        /// </summary>
         public void Unlock()
-        {            
-            throw new NotImplementedException();
+        {
+            UsingResource = NotUsed;
         }
     }
 }
---
+++ Mutex/Mutex/TestMutex.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mutex
+{
+    class TestMutex
+    {
+        private static Mutex Mut = new Mutex();
+        private const int TotalIterations = 3;
+        private const int TotalThreads = 10;
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < TotalThreads; i++)
+            {
+                Thread newThread = new Thread(new ThreadStart(ThreadProc))
+                {
+                    Name = string.Format("Thread {0}", i + 1)
+                };
+                newThread.Start();
+            }
+        }
+
+        private static void ThreadProc()
+        {
+            for (int i = 0; i < TotalIterations; i++)
+            {
+                DoSomething();
+            }
+        }
+
+        private static void DoSomething()
+        {
+            Console.WriteLine("{0} sent request", Thread.CurrentThread.Name);
+
+            Mut.Lock();
+            Console.WriteLine("{0} is in the protected code scope", Thread.CurrentThread.Name);
+
+            Thread.Sleep(50);
+
+            Console.WriteLine("{0} is out of the protected code scope", Thread.CurrentThread.Name);
+            Mut.Unlock();
+        }
+    }
+}
