8a472620c22c7afa072a4bdc23fe9c75749ee623
im1sha <vkporsche@ya.ru>
2018-10-12 13:27:10 +0300

    [Parallel]: class Parallel & example implemented

---
+++ Parallel/Parallel/Parallel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parallel
+{
+    public static class Parallel
+    {
+        /// <summary>
+        /// Class that stores data for action performing
+        /// </summary>
+        private class ActionData : IEnumerable
+        {
+            public ActionData(Action<object>[] Actions, object[] Objects)
+            {
+                int length = Math.Min(Actions.Length, Objects.Length);
+                for (int i = 0; i < length; i++)
+                {
+                    Data.Add((Actions[i], Objects[i]));
+                }
+            }
+
+            public readonly List<ValueTuple<Action<object>, object>> Data = 
+                new List<ValueTuple<Action<object>, object>>();
+
+            public IEnumerator GetEnumerator()
+            {
+                return ((IEnumerable)Data).GetEnumerator();
+            }
+        }
+
+        /// <summary>
+        /// Starts all delegates and waits they completed 
+        /// </summary>
+        /// <param name="actions">Array delegates to process</param>
+        /// <param name="objects">Data to be processed by delegates</param>
+        public static void WaitAll(Action<object>[] actions, object[] objects)
+        {
+            if (actions.Length != objects.Length)
+            {
+                throw new ArgumentException("Argumets should have the same length");
+            }
+
+            var data = new ActionData(actions, objects);
+
+            using (var finished = new CountdownEvent(1))
+            {
+                foreach (var a in data.Data)
+                {
+                    finished.AddCount(); 
+                    ThreadPool.QueueUserWorkItem(delegate {
+                        try
+                        {
+                            a.Item1(a.Item2);
+                        }
+                        finally
+                        {
+                            finished.Signal(); 
+                        }
+                    });
+                }
+                finished.Signal(); 
+                finished.Wait(); 
+            }
+        }
+    }
+}
--- Parallel/Parallel/Program.cs
+++ Parallel/Parallel/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,22 @@ namespace Parallel
     {
         static void Main(string[] args)
         {
+            WaitAllMethodExample();
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
         }
+
+        static void WaitAllMethodExample()
+        {
+            List<Action<object>> actions = new List<Action<object>>();
+            List<object> objects = new List<object>();
+            for (int i = 0; i < 20; i++)
+            {
+                Action<object> a = (o) => { Thread.Sleep(500); Console.WriteLine((int)o); };
+                actions.Add(a);
+                objects.Add(i);
+            }
+            Parallel.WaitAll(actions.ToArray(), objects.ToArray());
+        } 
     }
 }
