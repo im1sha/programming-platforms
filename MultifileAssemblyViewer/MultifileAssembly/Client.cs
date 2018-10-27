9d6a1e8678417892c9b9b501d59d5c0def139cb3
im1sha <vkporsche@ya.ru>
2018-10-27 06:45:28 +0300

    [multifileassemblyviewer]: project added

---
+++ MultifileAssemblyViewer/MultifileAssembly/Client.cs
@@ -0,0 +1,13 @@
+using System;
+using myStringer;
+
+namespace myClient
+{
+    public class Client
+    {
+        public void ClientMethod()
+        {			
+            System.Console.WriteLine("This is a line from ClientMethod.");
+        }
+    }
+}
---
+++ MultifileAssemblyViewer/MultifileAssemblyViewer/MultifileAssemblyViewer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MultifileAssemblyViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClient.Client client = new myClient.Client();
+            client.ClientMethod();
+
+            myStringer.Stringer stringer = new myStringer.Stringer();
+            stringer.StringerMethod();
+
+        }
+    }
+}
